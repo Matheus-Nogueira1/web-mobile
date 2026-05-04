d52f8d7b2bcef880aa9d377aea8421fe093152e6
Matheus Nogueira <138737937+Matheus-Nogueira1@users.noreply.github.com>
2026-05-04 16:36:17 -0300

    Começando Flutter
    começando a codar com flutter

--- .devcontainer/fundamentos_de_dart_dart/bin/olamundo.dart
+++ .devcontainer/fundamentos_de_dart_dart/bin/olamundo.dart
@@ -1,12 +1,37 @@
-/*importa a biblioteca */
-import 'dart:io';
-
-//Programa principal
-void main(){
-  //Exibe uma mensagem na tela e pula uma linha
-  print ('Faculdade de Tecnologia de Ourinhos');
-  print ('Análise e Desenvolovimento de Sistemas');
-
-  stdout.write('Técnicas Avançadas de Programação Web e Mobile \n');
-  stdout.write('Fundamentos de Dart');
+Class OlaMundoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Olá Mundo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Olá Mundo'),
+        ),
+        body: Center(
+          child: Text('Olá, Mundo!'),
+        ),
+      ),
+    );
+  }
 }
+Class OlaMundoPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return OlaMundoState();
+  } 
+}
+        appBar: AppBar(
+          title: Text('Olá Mundo'),
+        ),
+        body: Center(
+          child: Text('Olá, Mundo!'),
+        ),
+Class OlaMundoState extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState(){
+    return _OlaMundoStateState();
+  }
+}
+      body: Center(
+        child: Text('Olá, Mundo!'),
+      ),
